d5b84494005b8ef23e984d31d16709d8f5284f6f
central <central@think.yak.net>
2013-04-09 14:15:49 -0700

    Yak.java

---
+++ src/yak/etc/Yak.java
@@ -0,0 +1,84 @@
+package yak.etc;
+
+import java.util.HashMap;
+
+public abstract class Yak {
+	public static int ValueOfHexChar(char c) {
+		if ('0' <= c && c <= '9') {
+			return c - '0';
+		}
+		if ('A' <= c && c <= 'F') {
+			return c - 'A' + 10;
+		}
+		if ('a' <= c && c <= 'f') {
+			return c - 'a' + 10;
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public static String UrlDecode(String s) {
+		StringBuffer sb = new StringBuffer();
+		final int n = s.length();
+		for (int i = 0; i < n; i++) {
+			char c = s.charAt(i);
+			switch (c) {
+			case '+':
+				sb.append(" ");
+				break;
+			case '%':
+				if (i + 2 < n) {
+					char c1 = s.charAt(i + 1);
+					char c2 = s.charAt(i + 2);
+					int x = ValueOfHexChar(c1) * 16 + ValueOfHexChar(c2);
+					sb.append((char) x);
+					i += 2;
+				} else {
+					throw new IllegalArgumentException(s);
+				}
+				break;
+			default:
+				sb.append(c);
+				break;
+			}
+		}
+		return sb.toString();
+	}
+
+	public static String CurlyEncode(String s) {
+		final int n = s.length();
+		if (n == 0) {
+			return "{}"; // Special Case.
+		}
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < n; i++) {
+			char c = s.charAt(i);
+			if ('"' < c && c < '{') {
+				sb.append(c);
+			} else {
+				sb.append("{" + (int) c + "}"); // {%d}
+			}
+		}
+		return sb.toString();
+	}
+
+	public static String Show(HashMap<String, String> map) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("{map ");
+		for (String k : map.keySet()) {
+			sb.append("[ " + CurlyEncode(k) + " ]= " + CurlyEncode(map.get(k))
+					+ " ");
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+
+	public static String Show(String[] ss) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("{arr ");
+		for (int i = 0; i < ss.length; i++) {
+			sb.append("[" + i + "]= " + CurlyEncode(ss[i]) + " ");
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+}
